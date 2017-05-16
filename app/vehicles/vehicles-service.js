cd661e51e0cf28eb4408384b7d862111f7f9d524
kolekaramol <kolekaramol@hotmail.com>
2017-05-16 09:44:40 +0530

    added vehicle module

---
+++ app/vehicles/vehicles-service.js
@@ -0,0 +1,9 @@
+(function(){
+    angular.module("vehicles")
+    .service("vehicleSvc",["$http",
+                           function($http){
+        this.getVehicles=function(){
+         return  $http.get("app/api/vehicles.json");
+           };
+    }]);
+})();
